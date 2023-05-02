23f32f06039344e7cbba2085f5691fdaa7e8c7c2
KalfelsWiggers <gabriel_wiggers@estudante.sesisenai.org.br>
2023-05-02 09:10:43 -0300

    a

--- crud.php
+++ crud.php
@@ -8,14 +8,38 @@
     if(isset($_POST["gravar"]) )
     {
         $comando = $pdo->prepare("INSERT INTO alunos VALUE($matricula,'$nome',$idade)");
+        $resultado = $comando->execute();
+        header("Location: cadastro.html");
     }
 
     if(isset($_POST["excluir"]) )
     {
         $comando = $pdo->prepare("DELETE FROM alunos WHERE matricula=$matricula");
+        $resultado = $comando->execute();
+        header("Location: cadastro.html");
+    }
+
+    if(isset($_POST["alternar"]) )
+    {
+        $comando = $pdo->prepare("UPDATE alunos SET nome='$nome', idade=$idade WHERE matricula=$matricula");
+        $resultado = $comando->execute();
+        header("Location: cadastro.html");
     }
    
-    $resultado = $comando->execute();
+    if(isset($_POST["listar"]) )
+    {
+        $comando = $pdo->prepare("SELECT * FROM alunos");
+        $resultado = $comando->execute();
 
-    header("Location: cadastro.html");
+        while($linhas=$comando->fetch() )
+        {
+            $n = $linhas["nome"];
+            $m = $linhas["matricula"];
+            $i = $linhas["idade"];
+            echo
+            ("
+                $m $n $i <br> <br>
+            ");
+        }
+    }
 ?>
